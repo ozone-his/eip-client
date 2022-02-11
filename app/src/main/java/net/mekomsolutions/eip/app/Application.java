e57813cc41684ff5457746b1d4d2bb90079a481b
Emmanuel Nyachoke <achachiez@gmail.com>
2022-02-11 18:03:27 +0300

    TFS-284207: Update Springboot maven plugin version (#9)

--- app/src/main/java/net/mekomsolutions/eip/app/Application.java
+++ app/src/main/java/net/mekomsolutions/eip/app/Application.java
@@ -1,11 +1,14 @@
 package net.mekomsolutions.eip.app;
 
+import org.openmrs.eip.app.config.AppConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = {"org.openmrs.eip, net.mekomsolutions.eip.utils"})
+@Import(AppConfig.class)
 public class Application {
 	
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
