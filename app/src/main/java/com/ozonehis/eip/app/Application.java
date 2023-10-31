1fa93cbab3f3911bc23f4ec16ed2e235c580bd6d
corneliouzbett <kipchubett@gmail.com>
2023-10-31 14:31:09 +0300

    (docs): Update readme

--- app/src/main/java/com/ozonehis/eip/app/Application.java
+++ app/src/main/java/com/ozonehis/eip/app/Application.java
@@ -14,7 +14,7 @@ public class Application {
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 	
 	public static void main(final String[] args) {
-		logger.info("Starting application . . .");
+		logger.info("Starting EIP Client Application . . .");
 		
 		SpringApplication.run(Application.class, args);
 	}
