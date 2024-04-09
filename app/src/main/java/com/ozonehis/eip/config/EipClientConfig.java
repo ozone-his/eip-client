8f9356ca262b1bd197590dc7b09b39cf470e677f
Kipchumba C. Bett <kbett68@gmail.com>
2024-04-09 08:33:46 +0300

    Update Inception year to 2021.

--- app/src/main/java/com/ozonehis/eip/config/EipClientConfig.java
+++ app/src/main/java/com/ozonehis/eip/config/EipClientConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2023, Ozone HIS <info@ozone-his.com>
+ * Copyright © 2021, Ozone HIS <info@ozone-his.com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
