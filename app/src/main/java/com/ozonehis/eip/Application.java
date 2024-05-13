f2ef84450078ec989c7a7164acd9bc07cc804413
Siddharth Vaish <sidvaish97@gmail.com>
2024-05-13 13:23:42 +0530

    Add condition to skip liquibase force release if url is not set

--- app/src/main/java/com/ozonehis/eip/Application.java
+++ app/src/main/java/com/ozonehis/eip/Application.java
@@ -13,46 +13,37 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.lockservice.LockServiceFactory;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"org.openmrs.eip, com.ozonehis.eip"})
 public class Application {
+    @Value("${spring.mngt-datasource.jdbcUrl:#{null}}")
+    private String url;
 
-    @Value("${spring.liquibase.enabled}")
-    private boolean liquibaseEnabled;
+    @Value("${spring.mngt-datasource.username:#{null}}")
+    private String username;
 
-    @Qualifier("mgmt-datasource")
-    private DataSource dataSource;
+    @Value("${spring.mngt-datasource.password:#{null}}")
+    private String password;
 
     public static void main(final String[] args) {
         log.info("Starting EIP Client Application . . .");
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean(name = "mgmt-datasource")
-    @ConfigurationProperties(prefix = "spring.mngt-datasource")
-    public DataSource dataSource() throws ClassNotFoundException {
-        return DataSourceBuilder.create().build();
-    }
-
     @PostConstruct
     public void releaseLiquibaseLock() {
-        if (!liquibaseEnabled) {
-            log.error("Inside releaseLiquibaseLock return");
+        if (url == null || username == null || password == null) {
+            log.info("Skipping liquibase step: forceReleaseLock");
             return;
         }
-        try (Connection connection = dataSource.getConnection()) {
-            log.error("Adding test log user name {} driver name {}", dataSource.getConnection().getMetaData().getUserName(), dataSource.getConnection().getMetaData().getDriverName());
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
             Database database = DatabaseFactory.getInstance()
                     .findCorrectDatabaseImplementation(new JdbcConnection(connection));
             LockServiceFactory.getInstance().getLockService(database).forceReleaseLock();
