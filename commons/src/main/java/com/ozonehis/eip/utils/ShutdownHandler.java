0aa1c8c56e3fc177589dfdec507aa56faaf76eca
Siddharth Vaish <sidvaish97@gmail.com>
2024-05-17 17:23:42 +0530

    Refactored code from eip-client to eip-commons

--- commons/src/main/java/com/ozonehis/eip/utils/ShutdownHandler.java
+++ commons/src/main/java/com/ozonehis/eip/utils/ShutdownHandler.java
@@ -1,4 +1,11 @@
-package com.ozonehis.eip;
+/*
+ * Copyright © 2021, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.eip.utils;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
