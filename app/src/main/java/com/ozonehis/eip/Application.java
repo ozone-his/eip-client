0d75d6382789cd939d1d00d4e2a6eb017f8f5672
Kipchumba C. Bett <kbett68@gmail.com>
2024-07-22 18:09:07 +0300

    OZ-657: Add eip-client.properties property source

--- app/src/main/java/com/ozonehis/eip/Application.java
+++ app/src/main/java/com/ozonehis/eip/Application.java
@@ -10,8 +10,10 @@ package com.ozonehis.eip;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @Slf4j
+@PropertySource("classpath:eip-client.properties")
 @SpringBootApplication(scanBasePackages = "com.ozonehis.eip.*, ${eip.app.scan.packages}")
 public class Application {
 
